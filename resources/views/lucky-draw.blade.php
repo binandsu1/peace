3801c1c65559d5fdd27332a96319576bf4f54f51
jyfwq <jy@gmail.com>
2021-11-15 17:49:39 +0800

    www

--- resources/views/lucky-draw.blade.php
+++ resources/views/lucky-draw.blade.php
@@ -46,7 +46,7 @@
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"
         integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd"
 	crossorigin="anonymous"></script>
-<script src="<?=asset("/jy/awardRotate.js")?>"</script>
-<script src="<?=asset("/jy/lottery.js")?>"</script>
+<script src="<?=asset('/jy/awardRotate.js')?>"</script>
+<script src="<?=asset('/jy/lottery.js')?>"</script>
 </body>
 </html>
