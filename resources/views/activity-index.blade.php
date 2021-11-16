2daa26f8f1e58f394632b7a6953ff341db5aba40
jyfwq <jy@gmail.com>
2021-11-16 10:10:24 +0800

    sss

--- resources/views/activity-index.blade.php
+++ resources/views/activity-index.blade.php
@@ -1,36 +1,48 @@
-<!doctype html>
-<html lang="zh-CN">
+<!DOCTYPE html>
+<html lang="en" class="no-js">
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- 上述3个meta标签*必须*放在最前面，任何其他内容都*必须*跟随其后！ -->
-    <title>Bootstrap 101 Template</title>
+<meta charset="UTF-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge"> 
+<meta name="viewport" content="width=device-width, initial-scale=1"> 
+<title>login</title>
+<link rel="stylesheet" type="text/css" href="<?= asset('/jy/normalize.css') ?>">
+<link rel="stylesheet" type="text/css" href="<?= asset('/jy/demo.css') ?>">
+<!--必要样式-->
+<link rel="stylesheet" type="text/css" href="<?= asset('/jy/component.css') ?>">
 
-    <!-- Bootstrap -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
-
-    <!-- HTML5 shim 和 Respond.js 是为了让 IE8 支持 HTML5 元素和媒体查询（media queries）功能 -->
-    <!-- 警告：通过 file:// 协议（就是直接将 html 页面拖拽到浏览器中）访问页面时 Respond.js 不起作用 -->
-    <!--[if lt IE 9]>
-    <script src="https://cdn.jsdelivr.net/npm/html5shiv@3.7.3/dist/html5shiv.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/respond.js@1.4.2/dest/respond.min.js"></script>
-    <![endif]-->
+<!--[if IE]>
+<script src="<?=asset('/jy/html5.js')?>"></script>
+<![endif]-->
 </head>
 <body>
+		<div class="container demo-1">
+			<div class="content">
+				<div id="large-header" class="large-header">
+					<canvas id="demo-canvas"></canvas>
+					<div class="logo_box">
+						<h1>现在就<span style="color:red;">耀</span></h1>
+						<h3>点亮海报活动</h3><br><br><br>
+						<form action="#" name="f" method="post">
+							<!--<div class="input_outer">-->
+								<!--<span class="u_user"></span>-->
+								<!--<input name="logname" class="text" style="color: #FFFFFF !important" type="text" placeholder="请输入账户">-->
+							<!--</div>-->
+							<!--<div class="input_outer">-->
+								<!--<span class="us_uer"></span>-->
+								<!--<input name="logpass" class="text" style="color: #FFFFFF !important; position:absolute; z-index:100;"value="" type="password" placeholder="请输入密码">-->
+							<!--</div>-->
+							<div class="mb2"><a class="act-but submit" href="{{route('activity-up')}}" style="color: #FFFFFF">线上点亮</a></div><br>
+							<div class="mb2"><a class="act-but submit" href="{{route('activity-down')}}" style="color: #FFFFFF">线下点亮</a></div>
+						</form>
+					</div>
+				</div>
+			</div>
 
-<div  style="margin-left:45%">
-<h1 >活动主页</h1>
-<ul class="nav nav-pills text-center" >
-    <li role="presentation" class="active"><a href="{{route('activity-up')}}">活动 线上入口</a></li>
-    <li role="presentation" class="active"><a href="{{route('activity-down')}}">活动 线下入口</a></li>
-</ul>
-</div>
 
-
-<!-- jQuery (Bootstrap 的所有 JavaScript 插件都依赖 jQuery，所以必须放在前边) -->
-<script src="https://cdn.jsdelivr.net/npm/jquery@1.12.4/dist/jquery.min.js" integrity="sha384-nvAa0+6Qg9clwYCGGPpDQLVpLNn0fRaROjHqs13t4Ggj3Ez50XnGQqc/r8MhnRDZ" crossorigin="anonymous"></script>
-<!-- 加载 Bootstrap 的所有 JavaScript 插件。你也可以根据需要只加载单个插件。 -->
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
-</body>
+		</div><!-- /container -->
+		<script src="<?=asset('/jy/TweenLite.min.js')?>"></script>
+		<script src="<?=asset('/jy/EasePack.min.js')?>"></script>
+		<script src="<?=asset('/jy/rAF.js')?>"></script>
+		<script src="<?=asset('/jy/demo-1.js')?>"></script>
+	</body>
 </html>
