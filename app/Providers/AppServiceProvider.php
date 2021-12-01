98a2b65b393fef8070d7db61dd4a4813d6041c3c
yijun.yan <yijun.yan@xueda.com>
2021-12-01 15:16:44 +0800

    代码还原 侯密

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -31,9 +31,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(UrlGenerator $url)
     {
-        $env = env('APP_ENV');
-        if($env != 'local'){
-            $url->forceScheme('https');
-        }
+   
     }
 }
