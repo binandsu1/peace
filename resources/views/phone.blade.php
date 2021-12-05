13f4cbe994a1bb0b28aeab011caf595b6bafe4e1
yijun.yan <yijun.yan@xueda.com>
2021-12-05 22:09:29 +0800

    sfa

--- resources/views/phone.blade.php
+++ resources/views/phone.blade.php
@@ -1,47 +1,148 @@
-<!doctype html>
-<html lang="zh-CN">
+<!DOCTYPE html>
+<html lang="en" class="no-js">
 <head>
-    <meta charset="utf-8">
+    <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- 上述3个meta标签*必须*放在最前面，任何其他内容都*必须*跟随其后！ -->
-    <title>Bootstrap 101 Template</title>
+    <title>现在就耀，与现同框</title>
+    <link rel="stylesheet" type="text/css" href="<?= asset('/jy/normalize.css') ?>">
+    <link rel="stylesheet" type="text/css" href="<?= asset('/jy/demo.css') ?>">
+    <!--必要样式-->
+    <link rel="stylesheet" type="text/css" href="<?= asset('/jy/component.css') ?>">
+    <style>
+        .demo-1 .large-header {
+            background-image: url("<?= asset('/jy/主背景.jpg')?>");
+            background-size: 100vw 100vh;
+            width: 100vw;
+            height: 100vh;
+            background-repeat: no-repeat;
+            position: fixed;
+        }
 
-    <!-- Bootstrap -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"
-          integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+        .btn-1 {
+            width: 70vw;
+            height: 50vh;
+            padding: 1vh;
+            position: fixed;
+            overflow: hidden;
+            left: 50%;
+            top: 100%;
+            margin-left: -34vw;
+            margin-top: -17vh;
+        }
 
-    <!-- HTML5 shim 和 Respond.js 是为了让 IE8 支持 HTML5 元素和媒体查询（media queries）功能 -->
-    <!-- 警告：通过 file:// 协议（就是直接将 html 页面拖拽到浏览器中）访问页面时 Respond.js 不起作用 -->
-    <!--[if lt IE 9]>
-    <script src="https://cdn.jsdelivr.net/npm/html5shiv@3.7.3/dist/html5shiv.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/respond.js@1.4.2/dest/respond.min.js"></script>
+        .btn-2 {
+            width: 90vw;
+            height: 14vh;
+            padding: 1vh;
+            position: fixed;
+            overflow: hidden;
+            left: 50%;
+            top: 100%;
+            margin-left: -30vw;
+            margin-top: -80vh;
+        }
+        .btn-3 {
+            width: 84vw;
+            height: 50vh;
+            padding: 1vh;
+            position: fixed;
+            overflow: hidden;
+            left: 0;
+            top: 100%;
+            margin-left: 8vw;
+            margin-top: -70vh;
+            font-size: 1rem;
+            text-align: center;
+        }
 
+        @media only screen and (min-width: 375px) and (min-height: 812px) {
+            .demo-1 .large-header {
+                background-image: url("<?= asset('/jy/主背景.jpg')?>");
+                background-size: 100vw 100vh;
+                width: 100vw;
+                height: 100vh;
+                background-repeat: no-repeat;
+                position: fixed;
+            }
 
+            .btn-1 {
+                width: 70vw;
+                height: 14vh;
+                padding: 1vh;
+                position: fixed;
+                overflow: hidden;
+                left: 50%;
+                top: 100%;
+                margin-left: -34vw;
+                margin-top: -20vh;
+            }
+            .btn-2 {
+                width: 90vw;
+                height: 14vh;
+                padding: 1vh;
+                position: fixed;
+                overflow: hidden;
+                left: 50%;
+                top: 100%;
+                margin-left: -30vw;
+                margin-top: -80vh;
+            }
+            .btn-3 {
+                width: 84vw;
+                height: 50vh;
+                padding: 1vh;
+                position: fixed;
+                overflow: hidden;
+                left: 0;
+                top: 100%;
+                margin-left: 8vw;
+                margin-top: -74vh;
+                font-size: 1rem;
+                text-align: center;
+            }
+        }
+    </style>
+<!--[if IE]>
+    <script src="<?=asset('/jy/html5.js')?>"></script>
     <![endif]-->
 </head>
 <body>
+<div class="container demo-1">
 
-<div style="margin-left:30%">
-    <h1>phone</h1>
-    <input type="file" accept="image/*" capture="camera"> 相机
+    <div id="large-header" class="large-header">
+        <div class="logo_box">
 
-    <input type="file" accept="video/*" capture="camcorder"> 录像
+            {{--<div style="margin-left:10%">--}}
+            <form class="form-horizontal" method="post" action="<?=route('phone',['api_token'=>request('api_token')])?>" enctype="multipart/form-data">
+                <div class="mb2 btn-2">
+                    <input type="file" name="image" accept="image/*" capture="camera" style="color: #744323">
+                </div>
+                <div class="mb2 btn-3">
 
-    <input type="file" accept="audio/*" capture="microphone"> 系统相册
+                    {{--<?php--}}
+                    {{--if (empty($userinfo->path)) {--}}
+                    {{--?>--}}
+                    {{--<img class="img-rounded" src="https://mssocial.una-ad.com/jy/微软logo.png">--}}
+                    {{--<?php--}}
+                    {{--} else {--}}
+                    {{--?>--}}
+                    <img class="img-rounded" width="60%" height="60%" src="<?= asset('/offline/'.$userinfo->pic_name)?>">
+                    <!--                        --><?php
+                    //                        }
+                    //                        ?>
 
-    <input type="file" accept="image/*" multiple> 多选
+                </div>
+                {{--<button type="submit" class="btn btn-success">开始上传</button>--}}
+                <div class="mb2 btn-1">
+                    <button type="submit" class="act-but submit btn btn-success" style="color: #744323">照片上传/授权</button>
+                </div>
+            </form>
 
 
-</div>
+        </div>
+    </div>
 
-<!-- jQuery (Bootstrap 的所有 JavaScript 插件都依赖 jQuery，所以必须放在前边) -->
-<script src="https://cdn.jsdelivr.net/npm/jquery@1.12.4/dist/jquery.min.js"
-        integrity="sha384-nvAa0+6Qg9clwYCGGPpDQLVpLNn0fRaROjHqs13t4Ggj3Ez50XnGQqc/r8MhnRDZ"
-        crossorigin="anonymous"></script>
-<!-- 加载 Bootstrap 的所有 JavaScript 插件。你也可以根据需要只加载单个插件。 -->
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"
-        integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd"
-        crossorigin="anonymous"></script>
+</div><!-- /container -->
 </body>
 </html>
