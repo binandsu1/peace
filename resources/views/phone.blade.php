cc4a15e5efe7e773375b36be01b1fef724d45d40
yijun.yan <yijun.yan@xueda.com>
2021-12-05 21:57:20 +0800

    ff

--- resources/views/phone.blade.php
+++ resources/views/phone.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>现在就耀，与现同框</title>
- 
+
 
 <!--[if IE]>
     <script src="<?=asset('/jy/html5.js')?>"></script>
@@ -14,10 +14,9 @@
 <body>
 <div class="container demo-1">
 
-    <div id="large-header" class="large-header">
-        <div class="logo_box">
 
-            <div style="margin-left:10%">
+
+
                 <h1>phone</h1>
                 <form class="form-horizontal" method="post" action="<?=route('phone',['api_token'=>request('api_token')])?>" enctype="multipart/form-data">
 
@@ -28,11 +27,10 @@
                 </form>
                 <img width="300px" height="300px" src="<?= asset('/offline/'.$userinfo->pic_name)?>" class="img-rounded">
                 {{--  <img width="300px" height="300px" src="http://peace.local/{{$image->path}}" class="img-rounded">--}}
-            </div>
 
 
-        </div>
-    </div>
+
+
 
 </div><!-- /container -->
 </body>
