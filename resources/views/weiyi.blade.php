fadc9ce1ed254e8629480c0343395132ce769d45
yijun.yan <yijun.yan@xueda.com>
2021-12-06 16:37:32 +0800

    fff

--- resources/views/weiyi.blade.php
+++ resources/views/weiyi.blade.php
@@ -34,18 +34,20 @@
         });
         local.search(g);
         //经纬
-        var map = new BMap.Map("l-map");
-        var point = new BMap.Point(116.331398,39.897445);
+        var map = new BMap.Map("allmap");
+        var point = new BMap.Point(116.331398,36.897445);
         map.centerAndZoom(point,12);
 
         var geolocation = new BMap.Geolocation();
+// 开启SDK辅助定位
+        geolocation.enableSDKLocation();
         geolocation.getCurrentPosition(function(r){
             if(this.getStatus() == BMAP_STATUS_SUCCESS){
                 var mk = new BMap.Marker(r.point);
                 map.addOverlay(mk);
                 map.panTo(r.point);
-                theLocation(r.point.lng,r.point.lat);
-               // alert('您的位置：'+r.point.lng+','+r.point.lat);
+                alert('您的位置：'+r.point.lng+','+r.point.lat);
+                 theLocation(r.point.lng,r.point.lat);
             }
             else {
                 alert('failed'+this.getStatus());
