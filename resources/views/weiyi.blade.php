329d21fc7b6fc3ec2f31330e42da080a947c098a
yijun.yan <yijun.yan@xueda.com>
2021-12-06 16:29:52 +0800

    www

--- resources/views/weiyi.blade.php
+++ resources/views/weiyi.blade.php
@@ -3,8 +3,8 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
     <script type="text/javascript" src="//api.map.baidu.com/api?type=webgl&v=1.0&ak=6ho8muDORIA2j1TNIvyG5F8Now40OPLg"></script>
-    <script type="text/javascript" src="https://api.map.baidu.com/api?v=2.0&ak=6ho8muDORIA2j1TNIvyG5F8Now40OPLg"></script>
-    <script type="text/javascript" src="https://libs.baidu.com/jquery/1.7.2/jquery.min.js"></script>
+    <script type="text/javascript" src="http://api.map.baidu.com/api?v=2.0&ak=6ho8muDORIA2j1TNIvyG5F8Now40OPLg"></script>
+    <script type="text/javascript" src="http://libs.baidu.com/jquery/1.7.2/jquery.min.js"></script>
     <title>地图</title>
     <style type="text/css">
         body, html{width: 100%;height: 100%; margin:0;font-family:"微软雅黑";}
@@ -28,24 +28,28 @@
     function gs(g){
         //list
         var map = new BMapGL.Map("l-map");            // 创建Map实例
-
+        map.centerAndZoom(new BMapGL.Point(116.5233837, 39.91092455), 11);
+        var local = new BMapGL.LocalSearch(map, {
+            renderOptions: {map: map, panel: "r-result"}
+        });
+        local.search(g);
         //经纬
+        var map = new BMap.Map("l-map");
+        var point = new BMap.Point(116.331398,39.897445);
+        map.centerAndZoom(point,12);
+
         var geolocation = new BMap.Geolocation();
         geolocation.getCurrentPosition(function(r){
-
             if(this.getStatus() == BMAP_STATUS_SUCCESS){
                 var mk = new BMap.Marker(r.point);
                 map.addOverlay(mk);
                 map.panTo(r.point);
-                // alert('您的位置：'+r.point.lng+','+r.point.lat);
-                theLocation(r.point.lng,r.point.lat);
-                list(r.point.lng,r.point.lat);
+                alert('您的位置：'+r.point.lng+','+r.point.lat);
             }
             else {
                 alert('failed'+this.getStatus());
             }
         });
-        map.enableScrollWheelZoom(true);
 
         // 用经纬度设置地图中心点
         function theLocation(x,y) {
@@ -58,14 +62,6 @@
                 map.addOverlay(marker);              // 将标注添加到地图中
                 map.panTo(new_point);
             }
-        }
-
-        function list(x,y){
-            map.centerAndZoom(new BMapGL.Point(x, y), 12);
-            var local = new BMapGL.LocalSearch(map, {
-                renderOptions: {map: map, panel: "r-result"}
-            });
-            local.search(g);
         }
 
 
