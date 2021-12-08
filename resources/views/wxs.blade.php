8f42af4c344a4a6bc95ae2805c7565fd34aee5d1
yijun.yan <yijun.yan@xueda.com>
2021-12-08 15:08:25 +0800

    www

--- resources/views/wxs.blade.php
+++ resources/views/wxs.blade.php
@@ -1,23 +1,9 @@
 <?php
 require_once public_path("jssdk.php");//引入相应路径下的jssdk文件
-//appID
-//wx1647b3429377748f
-//appsecret
-//7c837f2ff2887845c13558742232a43d
 
-//第三方平台APPID：wx062a1b8f9e01e151
-//微信公众号APPID：wx2055b6314cd25ac1
-//skey：gb6hs2up
-
-//$jssdk = new JSSDK("wx062a1b8f9e01e151","gb6hs2up");//按照自己的公众号填写
-//$jssdk = new JSSDK("wx062a1b8f9e01e151","gb6hs2up");//按照自己的公众号填写
-//$signPackage = $jssdk->GetSignPackage();
-
-
-$jssdk = new JSSDK("wx062a1b8f9e01e151");//按照自己的公众号填写
+$jssdk = new JSSDK("wx2055b6314cd25ac1");//按照自己的公众号填写
 
 $signPackage = $jssdk->GetSignPackage();
-
 $data = array(
     'appId' => $signPackage["appId"],
     'nonceStr' => $signPackage["nonceStr"],
@@ -42,10 +28,9 @@ haha <button onclick="a()"></button>
 </html>
 <script src="https://res.wx.qq.com/open/js/jweixin-1.6.0.js"></script>
 <script>
-
     var url=window.location.href;
     wx.config({
-        debug:true,
+        debug:false,
         appId: '<?php echo $data["appId"];?>',
         timestamp: <?php echo $data["timestamp"];?>,
         nonceStr: '<?php echo $data["nonceStr"];?>',
@@ -73,20 +58,17 @@ haha <button onclick="a()"></button>
             // 如：{"checkResult":{"chooseImage":true},"errMsg":"checkJsApi:ok"}
         }
     });
-
-
     wx.ready(function () {      //需在用户可能点击分享按钮前就先调用
         wx.updateTimelineShareData({
-            title: '我是自定义文案', // 分享标题
-            link: 'https://mssocial.una-ad.com/api/wxs', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-            imgUrl: 'https://mssocial.una-ad.com/offline/176.jpg', // 分享图标
+            title: '微软零售全品牌大使李现的特殊海报上还有位置，等你哟！', // 分享标题
+            link: 'https://mssocial.una-ad.com/api/activityIndex?type=wx', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+            imgUrl: 'https://mssocial.una-ad.com/offline/share.jpg', // 分享图标
             success: function () {
-                alert('su');
+                // alert('su');
                 // 设置成功
             }
         })
     });
-
 
 </script>
 
