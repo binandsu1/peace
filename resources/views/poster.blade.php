1742a410635f5125e58d560835a1385c93d82c5e
yijun.yan <yijun.yan@xueda.com>
2021-12-08 17:45:02 +0800

    eew

--- resources/views/poster.blade.php
+++ resources/views/poster.blade.php
@@ -257,7 +257,7 @@ $data = array(
             link: 'https://mssocial.una-ad.com/api/activityIndex?type=wx', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
             imgUrl: 'https://mssocial.una-ad.com/offline/share.jpg', // 分享图标
             success: function () {
-                alert('su');
+                // alert('su');
                 // 设置成功
             }
         });
